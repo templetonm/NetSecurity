dcb93c8bf0051579391a4fe677bca49735bda063
Robert Sikorski <sikorski.rob@gmail.com>
2011-11-11 16:49:30 -0500

    Updated constants and comments to reflect new ports to be used. Fixed naming problem in hwClientStarter and hwServerStarter.

--- SikorskiCode/hwClientStarter.java
+++ SikorskiCode/hwClientStarter.java
@@ -3,16 +3,16 @@ import java.io.*;
 import java.net.*;
 import java.lang.*;
 
-class hw3{
+class hwClientStarter{
     // use localhost and an ssh tunnel instead.
     // tunnel with:
-    //  ssh -L 8170:helios.ececs.uc.edu:8170 notabad@helios.ececs.uc.edu
-    //  ssh -R 42849:localhost:42849 notabad@helios.ececs.uc.edu
+    //  ssh -L 8160:helios.ececs.uc.edu:8160 notabad@helios.ececs.uc.edu
+    //  ssh -R 42749:localhost:42749 notabad@helios.ececs.uc.edu
     //public static String MONITOR_NAME = "helios.ececs.uc.edu";
     public static String MONITOR_NAME = "localhost";
-    public static int MONITOR_PORT = 8170;
+    public static int MONITOR_PORT = 8160;
     public static String HOST_NAME = "localhost";
-    public static int LOCAL_PORT = 42849;
+    public static int LOCAL_PORT = 42749;
     
     public static void main(String[] args)
     {
