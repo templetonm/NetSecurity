551046dbc2acb2ffe1798feea3f3f344570d29b3
Robert Sikorski <sikorski.rob@gmail.com>
2011-11-13 21:36:01 -0500

    hwRSA initializes phiN, added constant for 1 as a BigInt.

--- SikorskiCode/hwRSA.java
+++ SikorskiCode/hwRSA.java
@@ -5,12 +5,15 @@ import java.util.*;
 
 public class hwRSA
 {
-    private static final numberOfBits = 32768; // 2^15
-
+    private static final int numberOfBits = 32768; // 2^15
+    private static final BigInteger ONE = new BigInteger("1");
+    
+    
     public Random savedRandom = null;
     public BigInteger p;
     public BigInteger q;
     public BigInteger n; // n = p * q
+    public BigInteger phiN;
     
     public hwRSA(Random random)
     {
@@ -19,6 +22,17 @@ public class hwRSA
         p = getPrime(random);
         q = getPrime(random);
         n = p.multiply(q);
+        phiN = (p.subtract(ONE)).multiply(q.subtract(ONE));
+    }
+    
+    public void initialize(Random random)
+    {
+        // initialize all the variables
+    }
+
+    public void load()
+    {
+        // load all the variables from a saved source
     }
     
     private BigInteger getPrime(Random random)
