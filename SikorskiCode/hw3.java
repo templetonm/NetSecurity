10609326df3b6b4679ebe1e014233ab5be0312e6
Robert Sikorski <sikorski.rob@gmail.com>
2011-11-13 19:46:10 -0500

    Now user_name and host_port are both taken as arguments.

--- SikorskiCode/hw3.java
+++ SikorskiCode/hw3.java
@@ -7,26 +7,27 @@ class hw3{
     // use localhost and an ssh tunnel instead.
     // tunnel with:
     //  ssh -L 8160:helios.ececs.uc.edu:8160 notabad@helios.ececs.uc.edu
-    //  ssh -R 42749:localhost:42749 notabad@helios.ececs.uc.edu
+    //  ssh -R 43049:localhost:43049 notabad@helios.ececs.uc.edu
     //public static String MONITOR_NAME = "helios.ececs.uc.edu";
     public static String MONITOR_NAME = "localhost";
     public static int MONITOR_PORT = 8160;
     public static String HOST_NAME = "localhost";
-    public static int LOCAL_PORT = 42749;
+//    public static int LOCAL_PORT = 43050;
     
     public static void main(String[] args)
     {
-        if (args.length != 1)
+        if (args.length != 2)
         {
-            System.out.println("\nbad args\n");
+            System.out.println("\nbad args");
+            System.out.println("usage:java hw3.java IDENT HOST_PORT\n");
         }
         else
         {
             try
             {
-                hwClient client = new hwClient(args[0], MONITOR_NAME, MONITOR_PORT, HOST_NAME, LOCAL_PORT);
+                hwClient client = new hwClient(args[0], MONITOR_NAME, MONITOR_PORT, HOST_NAME, Integer.parseInt(args[1]) );
                 client.start();
-                hwServer server = new hwServer(args[0], MONITOR_PORT, LOCAL_PORT);
+                hwServer server = new hwServer(args[0], MONITOR_PORT, Integer.parseInt(args[1]) );
                 server.start();
             }
             catch (Exception e){}
